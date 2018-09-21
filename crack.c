21cd692bdfa44e2b3b34d7fdc7ce1c951b57fdb7
b0b@c <cr17171700@163.com>
2018-09-21 11:15:34 +0800

    Update crack.c
    调试Hash值计算

--- crack.c
+++ crack.c
@@ -1,12 +1,14 @@
 #include <stdio.h>  
-#include <unistd.h>  
+#include <unistd.h>
+#include <string.h>
+#include <crypt.h>
 #include <stdlib.h>  
-#define _XOPEN_SOURCE  
+//#define _XOPEN_SOURCE  
   
 int main(int argc,char * argv[])  
 {  
         if (argc != 3){return -1;} 
-        char *passwd = crypt((const char *)argv[1],(const char *)argv[2]);  
-        printf("%sn",passwd);  
+        //char *passwd = crypt((const char *)argv[1],(const char *)argv[2]);  
+        printf("%s\n",crypt((const char *)argv[1],(const char *)argv[2]););  
         return 0;  
 }
